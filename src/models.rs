2a979866ae37ad395c12089a968300e9025cab77
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-06-25 23:23:23 +0300

    clean up

--- src/models.rs
+++ src/models.rs
@@ -11,19 +11,6 @@ pub struct SoftwareList {
     pub author: String,
 }
 
-
-/*impl From<software_list_models::Header> for NewSoftwareList {
-    fn from(header: software_list_models::Header) -> Self {
-        NewSoftwareList {
-            name: header.name,
-            description: header.description,
-            version: header.version,
-            author: header.author,
-        }
-    }
-}*/
-
-
 #[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
 #[diesel(belongs_to(SoftwareList))]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
@@ -35,18 +22,6 @@ pub struct Machine {
     pub software_list_id: i32,
 }
 
-/*impl From<software_list_models::Machine> for Machine {
-    fn from(machine: software_list_models::Machine) -> Self {
-        Machine {
-            id: None,
-            description: machine.description,
-            year: machine.year.map(|x| x as i32),
-            publisher: machine.publisher,
-            software_list_id: 0,
-        }
-    }
-}*/
-
 #[derive(Queryable, Selectable, Identifiable, Debug, PartialEq)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Rom {
@@ -57,17 +32,6 @@ pub struct Rom {
     pub sha1: String,
 }
 
-/*impl From<software_list_models::Rom> for Rom {
-    fn from(rom: software_list_models::Rom) -> Self {
-        Rom {
-            id: None,
-            name: rom.name,
-            size: rom.size as i32,
-            crc: rom.crc,
-            sha1: rom.sha1,
-        }
-    }
-}*/
 
 #[derive(Queryable, Selectable, Identifiable, Associations)]
 #[diesel(belongs_to(Machine))]
--- src/software_lists/fetch_software_list.rs
+++ src/software_lists/fetch_software_list.rs
@@ -8,8 +8,7 @@ use crate::database::roms::db_get_roms;
 pub fn fetch_software_list(connection: &mut SqliteConnection, id: i32) -> Result<models::SoftwareList, diesel::result::Error> {
 
     let software_list = db_get_software_list(connection, id)?;
-    //println!("{:?}", software_list);
-
+    println!("{:?}", software_list);
     let machines = db_get_machines(connection, &software_list)?;
     for machine in machines {
         println!("{:?}", machine);
