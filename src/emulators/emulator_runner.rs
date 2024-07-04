b824018d508546ffbd789c148e0aac3744b336aa
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-07-05 01:44:38 +0300

    feature: emulator runner

--- src/emulators/emulator_runner.rs
+++ src/emulators/emulator_runner.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
+use std::process::Command;
 use diesel::SqliteConnection;
 
 use crate::database::machines::db_get_machine;
+use crate::systems::systems::get_system_by_id;
 
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct Emulator{
@@ -18,10 +20,40 @@ pub fn run_with_emulator(conn: &mut SqliteConnection, system_id: String, emulato
     if !emulator.supported_systems.contains(&system_id) {
         return Err("System not supported by emulator".into());
     }
+    let system = get_system_by_id(system_id.clone())?;
     let machine = db_get_machine(conn, software_list_machine_id.clone())?;
+    let file_path = get_machine_file_path(&machine, &system)?;
+    println!("Running machine: {}", machine.name);
+    println!("File path: {}", file_path.clone());
+    println!("Running emulator {} with arguments {:?}", emulator.executable, emulator.arguments);
+    let output = Command::new(emulator.executable.clone())
+        .args(emulator.arguments.clone())
+        .arg(file_path.clone())
+        .output()?;
 
-    println!("Software list machine: {}", machine.name);
-    Ok(())
+    if output.status.success() == false {
+        let error_message = String::from_utf8(output.stderr).unwrap();
+        return Err(error_message.into());
+    } 
+
+    return Ok(());
+}
+
+fn get_machine_file_name(machine: &crate::models::Machine) -> String {
+    let mut filename = machine.name.clone();
+    filename.push_str(".zip");
+    filename
+}
+
+pub fn get_machine_file_path(machine: &crate::models::Machine, system: &crate::systems::systems::System) -> Result<String, Box<dyn Error>>{
+    let filename = get_machine_file_name(machine);
+    for path in system.file_paths.iter() {
+        let full_path = format!("{}/{}", path, filename);
+        if std::path::Path::new(&full_path).exists() {
+            return Ok(full_path);
+        }
+    }
+    return Err("Machine file not found".into());
 }
 
 pub fn get_emulator_by_id(emulator_id: String) -> Result<Emulator, Box<dyn Error>> {
