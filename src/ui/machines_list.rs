dd789663e28c4b9e2bcf5cd767e2e580c252fda3
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-07-11 12:18:07 +0300

    chore: format

--- src/ui/machines_list.rs
+++ src/ui/machines_list.rs
@@ -1,5 +1,5 @@
-use eframe::egui;
 use crate::models::Machine;
+use eframe::egui;
 
 pub struct MachinesList<'a> {
     ui: &'a mut egui::Ui,
@@ -27,29 +27,31 @@ impl<'a> MachinesList<'a> {
     }
 
     pub fn show(&mut self) {
-        egui::ScrollArea::vertical()
-            .show(self.ui, |ui| {
-                egui::Grid::new("machines_table").show(ui, |ui| {
-                    ui.label("Name");
-                    ui.label("Year");
-                    ui.end_row();
+        egui::ScrollArea::vertical().show(self.ui, |ui| {
+            egui::Grid::new("machines_table").show(ui, |ui| {
+                ui.label("Name");
+                ui.label("Year");
+                ui.end_row();
 
-                    for machine in self.machines.iter() {
-                        if ui.selectable_value(
+                for machine in self.machines.iter() {
+                    if ui
+                        .selectable_value(
                             self.selected_machine_id,
                             machine.id.clone(),
-                            machine.description.clone()
-                        ).clicked(){
-                            if *self.selected_machine_id != *self.previous_selected_machine_id {
-                                *self.new_selected_machine_id = Some(self.selected_machine_id.clone());
-                            }
+                            machine.description.clone(),
+                        )
+                        .clicked()
+                    {
+                        if *self.selected_machine_id != *self.previous_selected_machine_id {
+                            *self.new_selected_machine_id = Some(self.selected_machine_id.clone());
                         }
-                        ui.label(&machine.year.unwrap_or_default().to_string());
-                        ui.end_row();
                     }
-                });
+                    ui.label(&machine.year.unwrap_or_default().to_string());
+                    ui.end_row();
+                }
+            });
         });
-    }   
+    }
 }
 
 pub fn show_machines_list(
@@ -65,5 +67,6 @@ pub fn show_machines_list(
         selected_machine_id,
         previous_selected_machine_id,
         new_selected_machine_id,
-    ).show();
+    )
+    .show();
 }
